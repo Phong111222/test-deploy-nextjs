03243c873b5e29d2b26d965f3caaa557befdcb91
Tien Phong <tienphong111222@gmail.com>
2021-10-25 09:55:43 +0700

    delete code

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,6 @@
-import { useSession } from 'next-auth/client';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const [session, loading] = useSession();
-
   return (
     <div className={styles.container}>
       <h1>Homepage</h1>
